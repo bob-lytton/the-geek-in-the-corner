a04a5d836ddd0dbd0854b26d933d950be6bb7546
bob-lytton <39236531+bob-lytton@users.noreply.github.com>
2021-03-30 16:36:33 +0800

    test push

--- 01_basic-client-server/client.c
+++ 01_basic-client-server/client.c
@@ -206,8 +206,10 @@ void on_completion(struct ibv_wc *wc)
 {
   struct connection *conn = (struct connection *)(uintptr_t)wc->wr_id;
 
-  if (wc->status != IBV_WC_SUCCESS)
+  if (wc->status != IBV_WC_SUCCESS){
+    printf("wc_status: %d\n", wc->status);
     die("on_completion: status is not IBV_WC_SUCCESS.");
+  }
 
   if (wc->opcode & IBV_WC_RECV)
     printf("received message: %s\n", conn->recv_region);
